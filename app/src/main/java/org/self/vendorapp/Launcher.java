e9f76b846464e75d013a374c52fd41b07f09b3e2
Chirag Maheshwari <csoni111@gmail.com>
2016-09-27 20:35:24 +0530

    Added auto rotation and elevation in map using gyro.

--- app/src/main/java/org/self/vendorapp/Launcher.java
+++ app/src/main/java/org/self/vendorapp/Launcher.java
@@ -1,7 +1,11 @@
 package org.self.vendorapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Launcher extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class Launcher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        Button locateMe = (Button) findViewById(R.id.btLocate);
+        Button locateManual = (Button) findViewById(R.id.btManual);
+        locateMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Launcher.this,HomeScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        locateManual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Launcher.this,"Under Construction",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
